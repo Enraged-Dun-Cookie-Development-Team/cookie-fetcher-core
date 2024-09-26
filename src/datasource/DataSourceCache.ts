234f40946cff1392ef6421dde141b497f43eb5ce
Bosheng0422 <Sakuraisho0121@126.com>
2024-09-26 13:09:22 -0400

    follow上个commit

--- src/datasource/DataSourceCache.ts
+++ src/datasource/DataSourceCache.ts
@@ -1,44 +1,48 @@
+import { DataContentType, DataItem } from './DataItem';
+
 export class LRUCache {
-    private capacity: number;
-    private cache: Set<string>;
+  private capacity: number;
+  private cache: Set<string>;
 
-    constructor(capacity: number) {
-        this.capacity = capacity;
-        this.cache = new Set<string>();
-    }
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.cache = new Set<string>();
+  }
 
-    has(value: string): boolean {
-        return this.cache.has(value);
-    }
+  has(value: string): boolean {
+    return this.cache.has(value);
+  }
 
-    access(value: string): void {
-        if (this.cache.has(value)) {
-            this.cache.delete(value);
-        } else if (this.cache.size >= this.capacity) {
-            const oldestValue = this.cache.values().next().value as string;
-            this.cache.delete(oldestValue);
-        }
-        this.cache.add(value);
+  access(value: string): void {
+    if (this.cache.has(value)) {
+      this.cache.delete(value);
+    } else if (this.cache.size >= this.capacity) {
+      const oldestValue = this.cache.values().next().value as string;
+      this.cache.delete(oldestValue);
     }
+    this.cache.add(value);
+  }
 
-    listAccess(list: string[]): void {
-        list.forEach((value) =>{
-            this.access(value);
-        })
-    }
+  listAccess(list: string[]): void {
+    list.forEach((value) => {
+        this.access(value);
+      
+    });
+  }
 
-    printCache(): void {
-        console.log("Cache contents:");
-        this.cache.forEach(value => {
-            console.log(value);
-        });
-    }
+  dataItemListAccess(list: DataItem[]): void {
+    list.forEach((item) => {
+      if (item.type === DataContentType.COMMON) {
+        this.access(item.id);
+      }
+    });
+  }
 
-    getSize(): number {
-        return this.cache.size;
-    }
+  getSize(): number {
+    return this.cache.size;
+  }
 
-    getCacheList(): string[] {
-        return Array.from(this.cache);
-    }
+  getCacheList(): string[] {
+    return Array.from(this.cache);
+  }
 }
